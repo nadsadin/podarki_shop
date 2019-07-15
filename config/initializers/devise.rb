5c054db7268e61fc9677568694a271cbb3947ca9
Nadsadin <leloushlampergh@gmail.com>
2019-07-15 13:54:17 +0300

    set confirmable

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,10 +1,10 @@
 Devise.secret_key = "e4c5ff723edd6783f9824019cc6d3dd1dceaaa65becfe64be890d7c51d2e9f0a01ca00496928ba011905cfd16344c7563653"
-# Devise.setup do |config|
-#   # Required so users don't lose their carts when they need to confirm.
-#   config.allow_unconfirmed_access_for = 1.days
-#
-#   # Fixes the bug where Confirmation errors result in a broken page.
-#   config.router_name = :spree
-#
-#   # Add any other devise configurations here, as they will override the defaults provided by spree_auth_devise.
-# end
+Devise.setup do |config|
+  # Required so users don't lose their carts when they need to confirm.
+  config.allow_unconfirmed_access_for = 1.days
+
+  # Fixes the bug where Confirmation errors result in a broken page.
+  config.router_name = :spree
+
+  # Add any other devise configurations here, as they will override the defaults provided by spree_auth_devise.
+end
